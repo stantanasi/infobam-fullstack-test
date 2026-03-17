04ace2e98e0586b6ce8f368e6a421d80e51c5cd7
Lory-Stan TANASI <stantanasi@gmail.com>
2026-03-17 09:30:37 -0400

    feat: generate vehicles resource

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { VehiclesModule } from './vehicles/vehicles.module';
 
 @Module({
-  imports: [],
+  imports: [VehiclesModule],
   controllers: [AppController],
   providers: [AppService],
 })
