845af23991f63882b556c9dc55c9ccd8411099f0
Lory-Stan TANASI <stantanasi@gmail.com>
2026-03-17 12:13:37 -0400

    feat(/vehicles): navigate to vehicle details page

--- frontend/src/app/vehicles/page.tsx
+++ frontend/src/app/vehicles/page.tsx
@@ -1,4 +1,5 @@
 import { getVehicles } from '@/services/vehicle.service';
+import Link from 'next/link';
 
 export default async function Page() {
   const vehicles = await getVehicles();
@@ -6,7 +7,9 @@ export default async function Page() {
   return (
     <ul>
       {vehicles.map((vehicle) => (
-        <li key={vehicle.id}>{vehicle.manufacturer} {vehicle.model}</li>
+        <li key={vehicle.id}>
+          <Link href={`/vehicles/${vehicle.id}`}>{vehicle.manufacturer} {vehicle.model}</Link>
+        </li>
       ))}
     </ul>
   );
