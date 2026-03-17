d7139739131f6d411d0828aa26b6e44e47005210
Lory-Stan TANASI <stantanasi@gmail.com>
2026-03-17 14:38:07 -0400

    feat(vehicles): retrieve and display manufacturers

--- frontend/src/app/vehicles/page.tsx
+++ frontend/src/app/vehicles/page.tsx
@@ -1,16 +1,33 @@
+import { getManufacturers } from '@/services/manufacturer.service';
 import { getVehicles } from '@/services/vehicle.service';
 import Link from 'next/link';
 
 export default async function Page() {
+  const manufacturers = await getManufacturers();
   const vehicles = await getVehicles();
 
   return (
-    <ul>
-      {vehicles.map((vehicle) => (
-        <li key={vehicle.id}>
-          <Link href={`/vehicles/${vehicle.id}`}>{vehicle.manufacturer} {vehicle.model}</Link>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <div>
+        <div>
+          Marque :
+          <ul>
+            {manufacturers.map((manufacturer) => (
+              <li key={manufacturer.name}>{manufacturer.name}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      <div>
+        <ul>
+          {vehicles.map((vehicle) => (
+            <li key={vehicle.id}>
+              <Link href={`/vehicles/${vehicle.id}`}>{vehicle.manufacturer} {vehicle.model}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
