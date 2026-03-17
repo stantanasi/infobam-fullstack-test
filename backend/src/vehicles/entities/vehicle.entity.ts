d795bf3b4dbc9044551dff8ff4403427d200710d
Lory-Stan TANASI <stantanasi@gmail.com>
2026-03-17 09:37:49 -0400

    feat(vehicle.entity): add constructor to vehicle class

--- backend/src/vehicles/entities/vehicle.entity.ts
+++ backend/src/vehicles/entities/vehicle.entity.ts
@@ -1,4 +1,26 @@
-export class Vehicle {
+export interface IVehicle {
+  id: string;
+  manufacturer: string; // ex., "BMW", "Tesla", "Toyota"
+  model: string; // ex., "X5", "Model 3", "Camry"
+  year: number;
+  type: VehicleType;
+  price: number;
+  fuelType: FuelType;
+  transmission: string;
+  mileage?: number;
+  features: string[];
+  images: string[];
+  description: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export class Vehicle implements IVehicle {
+
+  constructor(data: IVehicle) {
+    Object.assign(this, data);
+  }
+
   id: string;
   manufacturer: string; // ex., "BMW", "Tesla", "Toyota"
   model: string; // ex., "X5", "Model 3", "Camry"
