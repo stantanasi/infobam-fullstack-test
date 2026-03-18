7092d1772a7773d37cd1ea5f83ba5cb19ece488a
Lory-Stan TANASI <stantanasi@gmail.com>
2026-03-18 12:08:46 -0400

    feat(vehicles): improve /vehicles page design

--- frontend/src/app/vehicles/page.tsx
+++ frontend/src/app/vehicles/page.tsx
@@ -1,6 +1,7 @@
 import { FuelType, FuelTypeLabels, VehicleType, VehicleTypeLabels } from '@/models/vehicle.model';
 import { getManufacturers } from '@/services/manufacturer.service';
 import { getVehicles } from '@/services/vehicle.service';
+import { Box, Checkbox, Container, Divider, Radio, Typography } from '@mui/material';
 import Link from 'next/link';
 
 const VEHICLES_LIMIT = 10;
@@ -18,6 +19,7 @@ export default async function Page(props: {
   searchParams: Promise<Partial<SearchParams>>;
 }) {
   const searchParams = await props.searchParams;
+  const currentPage = +(searchParams.page ?? 1);
 
   const sortBy = [
     {
@@ -49,10 +51,65 @@ export default async function Page(props: {
   });
 
   return (
-    <div>
-      <div>
-        Trier par :
-        <ul>
+    <Container
+      sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        gap: '20px',
+        padding: '10px 10px',
+      }}
+    >
+
+      <Box
+        sx={{
+          display: 'flex',
+          flex: 1,
+          flexDirection: 'column',
+          gap: '14px',
+        }}
+      >
+        <Link
+          href={`?`}
+          style={{ textDecoration: 'underline' }}
+        >
+          Réinitialiser
+        </Link>
+
+
+        <Box
+          sx={{
+            display: 'flex',
+            backgroundColor: '#ededed',
+            borderRadius: '8px',
+            flexDirection: 'column',
+            gap: '8px',
+            padding: '8px 16px',
+          }}
+        >
+          <Typography>
+            <Box sx={{ fontSize: '20px', fontWeight: 'bold' }}>
+              Trier par
+            </Box>
+          </Typography>
+          <Divider />
+          <Link
+            href={`?${new URLSearchParams({
+              ...searchParams,
+              sort: '',
+            }).toString()}`}
+          >
+            <Box
+              sx={{
+                alignItems: 'center',
+                display: 'flex',
+                flexDirection: 'row',
+                gap: '2px',
+              }}
+            >
+              <Radio checked={!searchParams.sort} sx={{ padding: 0 }} />
+              <Typography>Défaut</Typography>
+            </Box>
+          </Link>
           {sortBy.map((sort) => {
             const isSelected = searchParams.sort === sort.key;
             const newParams: Partial<SearchParams> = {
@@ -61,127 +118,323 @@ export default async function Page(props: {
             };
 
             return (
-              <li key={sort.key}>
-                <Link href={`?${new URLSearchParams(newParams).toString()}`}>
-                  {sort.label}
-                </Link>
-              </li>
+              <Link
+                key={sort.key}
+                href={`?${new URLSearchParams(newParams).toString()}`}
+              >
+                <Box
+                  sx={{
+                    alignItems: 'center',
+                    display: 'flex',
+                    flexDirection: 'row',
+                    gap: '2px',
+                  }}
+                >
+                  <Radio checked={isSelected} sx={{ padding: 0 }} />
+                  <Typography>{sort.label}</Typography>
+                </Box>
+              </Link>
             );
           })}
-        </ul>
-      </div>
+        </Box>
 
-      <div>
-        Filtres :
+        <Box
+          sx={{
+            display: 'flex',
+            backgroundColor: '#ededed',
+            borderRadius: '8px',
+            flexDirection: 'column',
+            gap: '14px',
+            padding: '12px',
+          }}
+        >
+          <Typography>
+            <Box sx={{ fontSize: '20px', fontWeight: 'bold' }}>
+              Filtres
+            </Box>
+          </Typography>
 
-        <div>
-          Marque :
-          <ul>
-            {manufacturers.map((manufacturer) => {
-              const isSelected = !!searchParams.manufacturer?.split(',').includes(manufacturer.name);
-              const newParams: Partial<SearchParams> = {
-                ...searchParams,
-                manufacturer: isSelected
-                  ? searchParams.manufacturer?.split(',').filter(v => v !== manufacturer.name).join(',')
-                  : [...(searchParams.manufacturer?.split(',') ?? []), manufacturer.name].join(','),
-              };
+          <Box
+            sx={{
+              backgroundColor: '#ffffff',
+              borderRadius: '8px',
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '8px',
+              padding: '8px 16px',
+            }}
+          >
+            <Typography>
+              Marque
+            </Typography>
+            <Divider />
+            <Box
+              sx={{
+                display: 'flex',
+                gap: '2px',
+                flexDirection: 'column',
+              }}
+            >
+              {manufacturers.map((manufacturer) => {
+                const isSelected = !!searchParams.manufacturer?.split(',').includes(manufacturer.name);
+                const newParams: Partial<SearchParams> = {
+                  ...searchParams,
+                  manufacturer: isSelected
+                    ? searchParams.manufacturer?.split(',').filter(v => v !== manufacturer.name).join(',')
+                    : [...(searchParams.manufacturer?.split(',') ?? []), manufacturer.name].join(','),
+                };
 
-              return (
-                <li key={manufacturer.name}>
-                  <Link href={`?${new URLSearchParams(newParams).toString()}`}>
-                    {manufacturer.name}
+                return (
+                  <Link
+                    key={manufacturer.name}
+                    href={`?${new URLSearchParams(newParams).toString()}`}
+                  >
+                    <Box
+                      sx={{
+                        alignItems: 'center',
+                        display: 'flex',
+                        flexDirection: 'row',
+                        gap: '2px',
+                      }}
+                    >
+                      <Checkbox checked={isSelected} sx={{ padding: 0 }} />
+                      <Typography>{manufacturer.name}</Typography>
+                    </Box>
                   </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+                );
+              })}
+            </Box>
+          </Box>
 
-        <div>
-          Type de voiture :
-          <ul>
-            {Object.values(VehicleType).map((type) => {
-              const isSelected = !!searchParams.type?.split(',').includes(type);
-              const newParams: Partial<SearchParams> = {
-                ...searchParams,
-                type: isSelected
-                  ? searchParams.type?.split(',').filter(v => v !== type).join(',')
-                  : [...(searchParams.type?.split(',') ?? []), type].join(','),
-              };
+          <Box
+            sx={{
+              backgroundColor: '#ffffff',
+              borderRadius: '8px',
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '8px',
+              padding: '8px 16px',
+            }}
+          >
+            <Typography>
+              Type de voiture
+            </Typography>
+            <Divider />
+            <Box
+              sx={{
+                display: 'flex',
+                gap: '2px',
+                flexDirection: 'column',
+              }}
+            >
+              {Object.values(VehicleType).map((type) => {
+                const isSelected = !!searchParams.type?.split(',').includes(type);
+                const newParams: Partial<SearchParams> = {
+                  ...searchParams,
+                  type: isSelected
+                    ? searchParams.type?.split(',').filter(v => v !== type).join(',')
+                    : [...(searchParams.type?.split(',') ?? []), type].join(','),
+                };
 
-              return (
-                <li key={type}>
-                  <Link href={`?${new URLSearchParams(newParams).toString()}`}>
-                    {VehicleTypeLabels[type]}
+                return (
+                  <Link
+                    key={type}
+                    href={`?${new URLSearchParams(newParams).toString()}`}
+                  >
+                    <Box
+                      sx={{
+                        alignItems: 'center',
+                        display: 'flex',
+                        flexDirection: 'row',
+                        gap: '2px',
+                      }}
+                    >
+                      <Checkbox checked={isSelected} sx={{ padding: 0 }} />
+                      <Typography>{VehicleTypeLabels[type]}</Typography>
+                    </Box>
                   </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+                );
+              })}
+            </Box>
+          </Box>
 
-        <div>
-          Carburant :
-          <ul>
-            {Object.values(FuelType).map((fuel) => {
-              const isSelected = !!searchParams.fuel_type?.split(',').includes(fuel);
-              const newParams: Partial<SearchParams> = {
-                ...searchParams,
-                fuel_type: isSelected
-                  ? searchParams.fuel_type?.split(',').filter(v => v !== fuel).join(',')
-                  : [...(searchParams.fuel_type?.split(',') ?? []), fuel].join(','),
-              };
+          <Box
+            sx={{
+              backgroundColor: '#ffffff',
+              borderRadius: '8px',
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '8px',
+              padding: '8px 16px',
+            }}
+          >
+            <Typography>
+              Carburant
+            </Typography>
+            <Divider />
+            <Box
+              sx={{
+                display: 'flex',
+                gap: '2px',
+                flexDirection: 'column',
+              }}
+            >
+              {Object.values(FuelType).map((fuel) => {
+                const isSelected = !!searchParams.fuel_type?.split(',').includes(fuel);
+                const newParams: Partial<SearchParams> = {
+                  ...searchParams,
+                  fuel_type: isSelected
+                    ? searchParams.fuel_type?.split(',').filter(v => v !== fuel).join(',')
+                    : [...(searchParams.fuel_type?.split(',') ?? []), fuel].join(','),
+                };
 
-              return (
-                <li key={fuel}>
-                  <Link href={`?${new URLSearchParams(newParams).toString()}`}>
-                    {FuelTypeLabels[fuel]}
+                return (
+                  <Link
+                    key={fuel}
+                    href={`?${new URLSearchParams(newParams).toString()}`}
+                  >
+                    <Box
+                      sx={{
+                        alignItems: 'center',
+                        display: 'flex',
+                        flexDirection: 'row',
+                        gap: '2px',
+                      }}
+                    >
+                      <Checkbox checked={isSelected} sx={{ padding: 0 }} />
+                      <Typography>{FuelTypeLabels[fuel]}</Typography>
+                    </Box>
                   </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      </div>
+                );
+              })}
+            </Box>
+          </Box>
 
-      <div>
-        Voitures :
-        <ul>
+        </Box>
+      </Box>
+
+
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          backgroundColor: '#ededed',
+          borderRadius: '8px',
+          flex: 3,
+          flexDirection: 'column',
+          gap: '20px',
+          padding: '20px 16px',
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '8px',
+          }}
+        >
           {vehicles.map((vehicle) => (
-            <li key={vehicle.id}>
-              <Link href={`/vehicles/${vehicle.id}`}>
-                {vehicle.manufacturer} {vehicle.model}
-              </Link>
-            </li>
+            <Link
+              key={vehicle.id}
+              href={`/vehicles/${vehicle.id}`}
+            >
+              <Box
+                sx={{
+                  display: 'flex',
+                  backgroundColor: '#ffffff',
+                  borderRadius: '8px',
+                  flexDirection: 'row',
+                }}
+              >
+                <Box
+                  component="img"
+                  sx={{
+                    width: '300px',
+                    height: '200px',
+                  }}
+                  src={vehicle.images[0]}
+                />
+
+                <Box
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '8px',
+                    justifyContent: 'space-between',
+                    padding: '20px 10px',
+                  }}
+                >
+                  <Box>
+                    <Typography>
+                      <Box sx={{ fontSize: '16px', fontWeight: 'bold' }}>
+                        {vehicle.manufacturer}
+                      </Box>
+                    </Typography>
+
+                    <Typography>
+                      {vehicle.model}
+                    </Typography>
+                  </Box>
+
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      flexDirection: 'row',
+                      gap: '30px',
+                    }}
+                  >
+                    <Typography>
+                      {vehicle.year}
+                    </Typography>
+
+                    <Typography>
+                      {VehicleTypeLabels[vehicle.type]}
+                    </Typography>
+
+                    <Typography>
+                      {FuelTypeLabels[vehicle.fuelType]}
+                    </Typography>
+                  </Box>
+
+                  <Typography variant="h4">
+                    {vehicle.price} €
+                  </Typography>
+                </Box>
+              </Box>
+            </Link>
           ))}
-        </ul>
-      </div>
+        </Box>
 
-      <div>
-        <Link href={(() => {
-          const currentPage = +(searchParams.page ?? 1);
-          const newParams: Partial<SearchParams> = {
-            ...searchParams,
-            page: (currentPage <= 1 ? 1 : currentPage - 1).toString(),
-          };
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            gap: '50px',
+          }}
+        >
+          <Link
+            href={`?${new URLSearchParams({
+              ...searchParams,
+              page: (currentPage <= 1 ? 1 : currentPage - 1).toString(),
+            }).toString()}`}
+            style={{
+              color: currentPage <= 1 ? '#d4d4de' : 'inherit',
+              pointerEvents: currentPage <= 1 ? 'none' : 'unset',
+            }}
+          >
+            {'<'}
+          </Link>
 
-          return `?${new URLSearchParams(newParams).toString()}`;
-        })()}>
-          {'<'}
-        </Link>
-
-        <Link href={(() => {
-          const currentPage = +(searchParams.page ?? 1);
-          const newParams: Partial<SearchParams> = {
-            ...searchParams,
-            page: (currentPage + 1).toString(),
-          };
-
-          return `?${new URLSearchParams(newParams).toString()}`;
-        })()}>
-          {'>'}
-        </Link>
-      </div>
-    </div>
+          <Link
+            href={`?${new URLSearchParams({
+              ...searchParams,
+              page: (currentPage + 1).toString(),
+            }).toString()}`}
+          >
+            {'>'}
+          </Link>
+        </Box>
+      </Box>
+    </Container>
   );
 }
